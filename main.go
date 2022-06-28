122736eff548c2c39e7c559cdea1b48560fdd212
Ben Allen <me@thebenallen.net>
2022-06-28 13:56:45 -0400

    Parse the config before anything concurrent starts

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"sync"
 
+	"gitlab.com/Njinx/instx/config"
 	"gitlab.com/Njinx/instx/proxy"
 	"gitlab.com/Njinx/instx/updater"
 )
 
 func main() {
+
+	// Parse the config before doing anything concurrent
+	config.ParseConfig()
 
 	var updatedCanidatesMutex sync.Mutex
 	updatedCanidates := updater.NewCanidates()
