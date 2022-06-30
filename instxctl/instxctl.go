b942d4927a10656352b50521eb54c45b78b0dd77
Ben Allen <me@thebenallen.net>
2022-06-30 00:17:53 -0400

    Can query instx instance and return stats

--- instxctl/instxctl.go
+++ instxctl/instxctl.go
@@ -1,6 +1,7 @@
 package instxctl
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -9,6 +10,7 @@ import (
 	"strings"
 
 	"gitlab.com/Njinx/instx/config"
+	"gitlab.com/Njinx/instx/updater"
 )
 
 func getInstXUrl() string {
@@ -42,8 +44,51 @@ func isInstanceRunning() bool {
 	return false
 }
 
+func getStats() updater.CanidatesMarshalable {
+	url := fmt.Sprintf("%s/stats", getInstXUrl())
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Could not fetch \"%s\": %s\n", url, err.Error())
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Could not read response body: %s\n", err.Error())
+	}
+
+	canidatesMarshalable := updater.CanidatesMarshalable{}
+	if err := json.Unmarshal(body, &canidatesMarshalable); err != nil {
+		log.Fatalln("Could not marshal stats JSON.")
+		os.Exit(1)
+	}
+
+	return canidatesMarshalable
+}
+
+func doStats() {
+	canidates := getStats()
+
+	fmt.Println(canidates)
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s COMMAND\n\n", os.Args[0])
+	fmt.Println("s|stats\tShow statistics about all instances")
+}
+
 func Run() {
 	instXUrl := getInstXUrl()
+
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "s", "stats":
+		doStats()
+	}
 
 	if !isInstanceRunning() {
 		fmt.Printf("It looks like InstX isn't running on \"%s\". Exiting.\n", instXUrl)
