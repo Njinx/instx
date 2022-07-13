41b0433488ae4b88aedd28678a17079fb27161fd
Ben Allen <me@thebenallen.net>
2022-07-13 19:31:45 -0400

    Removal notice in favor of Makefile

--- build.sh
+++ build.sh
@@ -1,24 +1,3 @@
 #!/bin/sh
 
-mkdir -p bin
-
-# _build OS ARCH EXT
-_build() {
-	os=$1
-	arch=$2
-	if [ -n "$3" ]; then
-		ext=".$3"
-	else
-		ext=""
-	fi
-
-	echo "Building: ${os}_${arch}"
-	env GOOS="$1" GOARCH="$2" go build -o "bin/instx-${os}-${arch}${ext}"
-}
-
-_build "linux" "amd64"
-_build "linux" "386"
-_build "windows" "amd64" "exe"
-_build "windows" "386" "exe"
-_build "darwin" "amd64"
-_build "darwin" "arm64"
+echo "[build.sh] This script was removed and replaced with Makefile. Please see the README."
